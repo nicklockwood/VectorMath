3ad1e33707f34ada673b02acb1269c699e9e7a3f
Harlan Haskins <harlan@harlanhaskins.com>
2018-05-07 16:46:48 -0400

    Split QuartzTests off from main tests

--- Tests/VectorMathTests/VectorMathTests.swift
+++ Tests/VectorMathTests/VectorMathTests.swift
@@ -6,10 +6,6 @@
 //  Copyright (c) 2014 Nick Lockwood. All rights reserved.
 //
 
-#if canImport(QuartzCore)
-import QuartzCore
-#endif
-
 import XCTest
 
 #if SWIFT_PACKAGE
@@ -43,35 +39,7 @@ class Vector2Tests: XCTestCase {
 }
 
 class Matrix3Tests: XCTestCase {
-    #if canImport(QuartzCore)
-    func testScale() {
-        
-        let transform = CGAffineTransform(scaleX: 0.3, y: 0.4)
-        let matrix = Matrix3(transform)
-        let compare = Matrix3(scale: Vector2(0.3, 0.4))
-        
-        XCTAssertTrue(matrix ~= compare)
-    }
-    
-    func testTranslation() {
-        
-        let transform = CGAffineTransform(translationX: 0.3, y: 0.4)
-        let matrix = Matrix3(transform)
-        let compare = Matrix3(translation: Vector2(0.3, 0.4))
-        
-        XCTAssertTrue(matrix ~= compare)
-    }
-    
-    func testRotation() {
-        
-        let transform = CGAffineTransform(rotationAngle: .pi / 2)
-        let matrix = Matrix3(transform)
-        let compare = Matrix3(rotation: .halfPi)
-        
-        XCTAssertTrue(matrix ~= compare)
-    }
-    #endif
-    
+
     func testMatrix3Multiplication() {
         
         let a = Matrix3(rotation: .halfPi)
@@ -104,35 +72,6 @@ class Matrix3Tests: XCTestCase {
 }
 
 class Matrix4Tests: XCTestCase {
-    
-    #if canImport(QuartzCore)
-    func testScale() {
-        
-        let transform = CATransform3DMakeScale(0.3, 0.4, 0.5)
-        let matrix = Matrix4(transform)
-        let compare = Matrix4(scale: Vector3(0.3, 0.4, 0.5))
-        
-        XCTAssertTrue(matrix ~= compare)
-    }
-    
-    func testTranslation() {
-        
-        let transform = CATransform3DMakeTranslation(0.3, 0.4, 0.5)
-        let matrix = Matrix4(transform)
-        let compare = Matrix4(translation: Vector3(0.3, 0.4, 0.5))
-        
-        XCTAssertTrue(matrix ~= compare)
-    }
-    
-    func testRotation() {
-        
-        let transform = CATransform3DMakeRotation(.pi / 2, 1, 0, 0)
-        let matrix = Matrix4(transform)
-        let compare = Matrix4(rotation: Vector4(1, 0, 0, .halfPi))
-        
-        XCTAssertTrue(matrix ~= compare)
-    }
-    #endif
     
     func testRotationAndTranslation() {
         let point = Vector4(0.0, -1.0, 0.0, 1.0)
